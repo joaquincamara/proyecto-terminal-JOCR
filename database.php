98f8bd900c998f80bff0ba2e02e19c8b03646b75
Joaquin Camara <jcamara@NSL-JCAMARAs-MacBook-Pro.local>
2020-03-14 11:09:09 -0700

    prod and dev settings

--- database.php
+++ database.php
@@ -1,9 +1,20 @@
 <?php
 
+// Production
+
+/*
 $server = 'localhost';
 $username = 'id12754084_joaquincamara';
 $password = 'Joaquincr1';
 $database = 'id12754084_users';
+*/
+
+// Develop
+$server = 'localhost';
+$username = 'id12754084_joaquincamara';
+$password = 'Joaquincr1';
+$database = 'id12754084_users';
+
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
