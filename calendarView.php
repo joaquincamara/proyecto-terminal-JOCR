ab0e976c05f2c0088ab1c46b88e50c43a0fc06f2
Joaquin Camara <jcamara@NSL-JCAMARAs-MacBook-Pro.local>
2020-05-18 20:32:55 -0500

    working on the calendar view and crud

--- calendarView.php
+++ calendarView.php
@@ -11,21 +11,21 @@
 <?php require 'partials/header.php' ?>
   <div class="calendarAndScheduleContainer">
     <div class="scheduleCreatorAndTableContainer">
-      <div class="clientRegistryContainer">
+      <div class="scheduleCreator">
         <form  method="POST">
           <div class="clientRegistryInputsSection">
             <div class="clientRegistryInputsSection-box1">
-              <input required id="completeName" placeholder="Nombre completo" name="completeName" type="text" >
-              <input required id="startTreatmentDate" placeholder="Inicio de tratamiento" name="startTreatmentDate" type="text"  >
+              <input required id="personToScheduleDate" placeholder="Agendar cita con:" name="personToScheduleDate" type="text" >
+              <input required id="scheduleTime" placeholder="Horario de cita" name="scheduleTime" type="text"  >
             </div>
             <div class="clientRegistryInputsSection-box2">
             <input required pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" id="email" placeholder="Correo electrónico" name="email" type="text" id="email" >
-              <input required id="endTreatmentDate" placeholder="Fin de tratamiento" name="endTreatmentDate" type="text" >
+              <input required id="scheduleDate" placeholder="Fecha de cita" name="endTrescheduleDateatmentDate" type="text" >
             </div>
           </div>
           <div class="clientRegistryTextareaSection">
-          <textarea required id="prescriptionDrugs" placeholder="Medicamentos" name="prescriptionDrugs"  rows="8"></textarea>  
-            <input required type="submit" value="Guardar cliente">
+          <textarea required id="specifications" placeholder="Especificaciones" name="specifications"  rows="4"></textarea>  
+            <input required type="submit" value="Guardar y enviar cita">
           </div>
         </form>
       </div>
